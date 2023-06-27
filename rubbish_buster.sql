c01e1adb2a6e9f26cf97e44ec1dbd7dbcc17dd32
Zaim1711 <108624495+Zaim1711@users.noreply.github.com>
2023-06-27 17:06:16 +0700

    Add files via upload

--- rubbish_buster.sql
+++ rubbish_buster.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 23 Jun 2023 pada 18.37
+-- Waktu pembuatan: 27 Jun 2023 pada 12.05
 -- Versi server: 10.4.27-MariaDB
 -- Versi PHP: 8.2.0
 
@@ -24,28 +24,75 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `locations`
+--
+
+CREATE TABLE `locations` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `latitude` varchar(20) DEFAULT NULL,
+  `longitude` varchar(20) DEFAULT NULL,
+  `photo_path` varchar(255) DEFAULT NULL,
+  `status` enum('pending','approved','rejected') DEFAULT 'pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `locations`
+--
+
+INSERT INTO `locations` (`id`, `name`, `address`, `latitude`, `longitude`, `photo_path`, `status`) VALUES
+(14, 'kontol', 'Jl.Ketintang Madya', '-7.261627', '112.750302', '212f4633-e83d-4dda-9817-4a619b9c98b6.JPG', 'approved');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `user`
 --
 
 CREATE TABLE `user` (
   `username` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `phone_num` varchar(20) DEFAULT NULL,
+  `photo_path` varchar(255) DEFAULT NULL,
+  `role` varchar(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `user`
 --
 
-INSERT INTO `user` (`username`, `email`, `password`) VALUES
-('Muhammad Zaim Wibowo Ramadhani', 'muhamadzaim.w@gmail.com', 'Muhamadzaim1711'),
-('Dhio', 'Dhio@gmail.com', 'Dhio12345'),
-('Nocz', 'Nocz@gmail.com', 'Nocz23345'),
-('test', 'albarmaula@gmail.com', 'Wam12345678'),
-('Saddam', 'Saddam@gmail.com', 'Saddam12345'),
-('HAYOLOH', 'jkabdj@gmail.com', 'Hayolo1234'),
-('Widad', 'Widad@gmail.com', 'Widad12345'),
-('Rafly', 'Rafly@gmail.com', 'Rafly12345');
+INSERT INTO `user` (`username`, `email`, `password`, `address`, `phone_num`, `photo_path`, `role`) VALUES
+('Widad123', 'Admin123@gmail.com', '$2y$10$062ZMLo4UYWc0BCDeLUHIuE.zLJvsShBUL6thl0OPP6dF.fB26NwO', '', '', '', 'admin'),
+('Muhammad Zaim Wibowo Ramadhani', 'muhamadzaim.w@gmail.com', '$2y$10$xB1g275TB2EABU.OJbs5xOkzcNnKhe2YZKVASfPznXBu/zWuP/Qbm', '', '', '', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `locations`
+--
+ALTER TABLE `locations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `locations`
+--
+ALTER TABLE `locations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
